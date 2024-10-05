78b57e66794437dfc59860895ee7818bed2cfeb3
un-earthly <vijayalamin@gmail.com>
2024-10-06 05:39:30 +0600

    trying to find hydration error

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+    SidebarLayout,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
 
-export default function layout() {
-  return (
-    <div>layout</div>
-  )
+export default async function Page({ children }) {
+    const { cookies } = await import("next/headers")
+    return (
+        (<SidebarLayout defaultOpen={cookies().get("sidebar:state")?.value === "true"}>
+            <AppSidebar />
+            <main
+                className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
+                <div className="h-full rounded-md border-2 border-dashed p-2">
+                    <SidebarTrigger />
+                    {children}
+                </div>
+            </main>
+        </SidebarLayout>)
+    );
 }
