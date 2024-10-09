582cc0f42e73e7d1f0e3e7f95a522329fe6213a8
un-earthly <vijayalamin@gmail.com>
2024-10-09 13:42:29 +0600

    added sha

--- src/components/HeroCards.tsx
+++
@@ -1,8 +0,0 @@
-
-export const HeroCards = () => {
-  return (
-    <div className="bg-[url('https://pathao.com/bn/wp-content/themes/webpathao/assets/images/mobile-screen-2.png')] h-[800px] w-[500px] flex items-center justify-center">
-      <img src="/public/banner.png" height="60%" width="66%" className="ml-16" alt="" />
-    </div>
-  );
-};
